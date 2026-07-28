6451c2e147b9e240434ad5481fbbcfaf3423137e
Michelle Belli <progweb0149@gmail.com>
2026-07-27 21:33:22 -0300

    exemplos

---
+++ funcoes_chamadas.php
@@ -0,0 +1,15 @@
+<?php
+
+//Chamada das funções que eu criei
+$resultado = 1250.55;
+echo (formatarMoedaBr($resultado));
+
+quebrarLinha();
+
+echo(calcularImc(60,1.70));
+quebrarLinha();
+echo(calcularImc(79,1.70));
+quebrarLinha();
+echo(calcularImc(110,1.70));
+quebrarLinha();
+echo(calcularImc(150,1.70));
--- funcoes_criadas.php
+++ funcoes_criadas.php
@@ -34,18 +34,3 @@ function calcularImc(float $peso, float $altura){
 function quebrarLinha(){
     echo("<br> <hr>");
 }
-
-
-//Chamada das funções que eu criei
-$resultado = 1250.55;
-echo (formatarMoedaBr($resultado));
-
-quebrarLinha();
-
-echo(calcularImc(60,1.70));
-quebrarLinha();
-echo(calcularImc(79,1.70));
-quebrarLinha();
-echo(calcularImc(110,1.70));
-quebrarLinha();
-echo(calcularImc(150,1.70));
